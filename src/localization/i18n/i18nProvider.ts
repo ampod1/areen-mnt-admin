f4bf3c7a1bee9b3089156ca096c1decafbd8fff3
Ahmed Sarhan <ahmedsarhan1515@gmail.com>
2021-07-28 22:14:42 +0200

    fixing ra-lng-arabic

--- src/localization/i18n/i18nProvider.ts
+++ src/localization/i18n/i18nProvider.ts
@@ -1,12 +1,11 @@
 import polyglotI18nProvider from 'ra-i18n-polyglot';
-import ArabicMessages from 'ra-language-arabic';
+// what I removed
+// import ArabicMessages from 'ra-language-arabic';
 import englishMessages from 'ra-language-english';
 import { resolveBrowserLocale } from 'react-admin';
+// what I added
+import ArabicMessages from '../Ar/ra-lang';
 import { arabicTranslations, englishTranslations } from '../index';
-// const i18nProvider = polyglotI18nProvider(locale =>
-//     locale === 'ar' ? ArabicMessages : englishMessages,
-//     'en' // Default locale
-// );
 
 const getDefaultLocale = () => {
 	let defaultLocale: string | undefined;
@@ -26,7 +25,10 @@ const i18nProvider = polyglotI18nProvider((locale) => {
 		return { ...englishMessages, custom_root: { ...englishTranslations } };
 	}
 	if (locale === 'ar') {
-		return { ...ArabicMessages, custom_root: { ...arabicTranslations } };
+		return {
+			...ArabicMessages,
+			custom_root: { ...arabicTranslations },
+		};
 	} else {
 		return englishMessages;
 	}
