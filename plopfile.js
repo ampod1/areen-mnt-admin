849162623e012ef671038a9186fb4b84ea0b1361
Momen <abdelmomen1985@gmail.com>
2021-09-02 20:43:18 +0200

    some great plops

--- plopfile.js
+++ plopfile.js
@@ -4,6 +4,7 @@ module.exports = function (
 ) {
   const { createApolloClient } = require("./src/plops/graphqlSetups");
   const { GET_RESOURCE_FIELDS } = require("./src/plops/graphQueries");
+  const { createElementFromColumn } = require("@abdelmomen1985/ra-plop-utils");
   // plop generator code
   plop.setGenerator("list", {
     description: "this is a skeleton plop react admin list file",
@@ -14,13 +15,29 @@ module.exports = function (
       async function getResource(answers) {
         const client = createApolloClient();
         const { data } = await client.query({ query: GET_RESOURCE_FIELDS });
-        console.log("resources", answers.resource);
-        console.log("data", data);
+        const { resource } = answers;
+        //console.log("data", data);
         if (data?.__type?.fields) {
           // Do
         }
-        const getTableFields = require("./src/plops/rest/getTableFields");
-        await getTableFields();
+        // TODO ts types is a must
+        const {
+          getTableFields,
+          getTableComments,
+          getTableForeignKeys,
+        } = require("./src/plops/rest/tableInfo");
+
+        const { result: fieldsResult } = await getTableFields(resource);
+        const { result: commentResult } = await getTableComments(resource);
+        const { result: foreignResult } = await getTableForeignKeys(resource);
+
+        const [_ignore_cols, ...cols] = fieldsResult;
+        const [_ignore_tableComments, tableComments] = commentResult;
+        const [_ignore_foreign, ...foreignKeys] = foreignResult;
+
+        console.log(cols, tableComments, foreignKeys);
+
+        console.log(createElementFromColumn({ column_name: cols[1][0] }));
         return "getResource";
       },
       "Done with actions",
